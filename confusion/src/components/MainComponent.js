b58a22ab98a54820276234f8150010dce4614e5b
RashakDude <rajatagarwal686@gmail.com>
2020-05-26 22:25:57 +0530

    Week 2

---
+++ confusion/src/components/MainComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponents';
+import Home from './HomeComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import DishDetail from './DishDetailComponent';
+import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES
+    };
+  }
+
+
+  render() {
+
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
+
+    return (
+      <div>
+        <Header/>
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default Main;
